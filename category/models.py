fe5b65c2720a1eba43c6a6eeb4935bda84c4db5d
OmuraCode <akyl.net@gmail.com>
2023-06-26 21:13:09 +0600

    first push

--- category/models.py
+++ category/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=75, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='children', blank=True, null=True)
+
+    def str(self):
+        return f'{self.name} -> {self.parent}' if self.parent else f'{self.name}'
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
--- emarket/urls.py
+++ emarket/urls.py
@@ -17,6 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/accounts/', include('account.urls')),
+    path('api/v1/category/', include('category.urls')),
+    path('api/v1/posts/', include('posts.urls')),
+    path('api/v1/comments/', include('comment.urls')),
+    path('api/v1/likes/', include('like.urls')),
+
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
