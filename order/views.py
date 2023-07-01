112c23117f97e6a181833e1536f72aa893bbf830
Arthur-KINg125 <artagg126@gmail.com>
2023-07-01 17:21:25 +0600

    added somifoahfoaf a

--- order/views.py
+++ order/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from . import serializers
+from .models import Post, Order
+from posts.permissions import IsAuthor
+
+
+class OrderCreateView(generics.ListCreateAPIView):
+
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = serializers.OrderUserSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+
