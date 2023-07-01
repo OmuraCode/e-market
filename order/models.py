cf899c3d604d1cfd6c8db24d36a843b4b6b409d4
Arthur-KINg125 <artagg126@gmail.com>
2023-07-01 17:39:37 +0600

    added something

--- order/models.py
+++ order/models.py
@@ -7,6 +7,8 @@ class Order(models.Model):
     owner = models.ForeignKey('account.CustomUser', related_name='orders', on_delete=models.CASCADE)
     post = models.ForeignKey(Post, related_name='orders', on_delete=models.CASCADE)
 
+
+
     class Meta:
         verbose_name = 'order'
         verbose_name_plural = 'orders'
